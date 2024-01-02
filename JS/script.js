957bfd19716604d00ebd811190d8d3f21b971f98
smitankhiwala <smitank102@gmail.com>
2024-01-02 23:22:32 +0530

    modified script.js file

---
+++ JS/script.js
@@ -0,0 +1,67 @@
+// Arithmetic Operators
+let a=6;
+let b=5;
+
+console.log("a=",a,"& b=",b);
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log("a**b=",a**b);
+
+// Unary Operators
+// a++;
+// console.log("a=",a);
+
+// a--;
+// console.log("a=",a);
+
+// console.log("++a=",++a);
+// console.log("a++=",a++);
+// console.log("a=",a);
+
+// console.log("--a=",--a);
+// console.log("a--=",a--);
+// console.log("a=",a);
+
+// Assignment Operators
+// a+=4;
+// console.log("a=",a);
+
+// a-=4;
+// console.log("a=",a);
+
+// a*=4;
+// console.log("a=",a);
+
+// a/=4;
+// console.log("a=",a);
+
+// a%=4;
+// console.log("a=",a);
+
+// a**=4;
+// console.log("a=",a);\
+
+// Comparison Operators
+// console.log("a==b",a==b);
+// console.log("a!=b",a!=b);
+// console.log("a>=b",a>=b);
+// console.log("a<=b",a<=b);
+// console.log("a>b",a>b);
+// console.log("a<b",a<b);
+
+// let c=5; //number
+// let d="5"; //string->number
+// console.log("c==d",c==d);
+// console.log("c===d",c===d);
+// console.log("c!==d",c!==d);
+
+// Logical Operators
+let cond1=a>b;
+let cond2=a===6;
+console.log("cond1 && cond2=",cond1 && cond2);
+console.log("cond1 || cond2=",cond1 || cond2);
+console.log("!(a<b)=",!(a<b));
+console.log("!(a===6)=",!(a===6));
