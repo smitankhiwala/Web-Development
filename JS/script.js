7021f02bac1a7ea85ada4e797fdd1f6c3f6ad4f3
smitankhiwala <smitank102@gmail.com>
2024-01-03 23:53:56 +0530

    modified script.js file

--- JS/script.js
+++ JS/script.js
@@ -59,9 +59,59 @@ console.log("a=",a,"& b=",b);
 // console.log("c!==d",c!==d);
 
 // Logical Operators
-let cond1=a>b;
-let cond2=a===6;
-console.log("cond1 && cond2=",cond1 && cond2);
-console.log("cond1 || cond2=",cond1 || cond2);
-console.log("!(a<b)=",!(a<b));
-console.log("!(a===6)=",!(a===6));
+// let cond1=a>b;
+// let cond2=a===6;
+// console.log("cond1 && cond2=",cond1 && cond2);
+// console.log("cond1 || cond2=",cond1 || cond2);
+// console.log("!(a<b)=",!(a<b));
+// console.log("!(a===6)=",!(a===6));
+
+// Ternary Operators
+let age=25;
+age>=18? console.log("adult") : console.log("not adult");
+// let res=age>=18? "adult" : "not adult";
+// console.log(res);
+
+// Conditional Statements
+// let age=18;
+// if(age>=18) {
+//     console.log("you can vote");
+// }
+
+// if(age<18) {
+//     console.log("you can't vote");
+// }
+
+let mode="silver";
+let color;
+// if(mode=="dark") {
+//     color="black";
+// } else {
+//     color="white";
+// }
+// console.log(color);
+
+if(mode==="dark") {
+    color="black";
+} else if(mode==="blue") {
+    color="blue";
+} else if(mode==="pink") {
+    color="pink";
+} else {
+    color="white";
+}
+console.log(color);
+
+// one time popup
+// alert("hello");
+
+// let name=prompt("hello");
+// console.log(name);
+
+let num=prompt("Enter a number:");
+console.log(num);
+if(num%5===0) {
+    console.log(num,"is a multiple of 5");
+} else {
+    console.log(num,"is not a multiple of 5");
+}
