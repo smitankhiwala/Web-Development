110860f0d578c4bf14547bbbd8ff0134cfc814b0
smitankhiwala <smitank102@gmail.com>
2024-01-08 19:09:27 +0530

    created arrays.js file

---
+++ JS/arrays.js
@@ -0,0 +1,11 @@
+// array is a special type of object and instead of key we use index
+let marks=[97,82,75,64,36];
+console.log(marks);
+console.log(marks.length);
+
+let heroes=["batman","superman","wonder woman","green lantern","flash","cyborg","martian manhunter","aquaman"];
+console.log(heroes);
+console.log(heroes.length);
+
+marks[0]=66;
+console.log(marks);
